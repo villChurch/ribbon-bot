fefc2906c3a265654c89f374d1ded59e3dfc0170
Will Spires <will@williamspires.com>
2023-07-09 11:54:35 +0100

    Move to chewtils for slash commands

--- src/main/java/com/villchurch/eponabot/Bot.java
+++ src/main/java/com/villchurch/eponabot/Bot.java
@@ -3,37 +3,28 @@ package com.villchurch.eponabot;
 import com.jagrosh.jdautilities.command.CommandClient;
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.command.SlashCommand;
-import com.villchurch.eponabot.Listeners.GuildMemberListener;
-import com.villchurch.eponabot.Listeners.InteractionListener;
-import com.villchurch.eponabot.Listeners.SlashListener;
 import com.villchurch.eponabot.slashcommands.EyeSpyCommands;
+import com.villchurch.eponabot.slashcommands.ProfileCommand;
+import com.villchurch.eponabot.slashcommands.RibbonCommands;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.Permission;
 import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
-import net.dv8tion.jda.api.interactions.commands.OptionType;
-import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.requests.GatewayIntent;
-import net.dv8tion.jda.api.requests.restaction.CommandListUpdateAction;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.stereotype.Service;
+import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.EnumSet;
 import java.util.List;
 
-import static net.dv8tion.jda.api.interactions.commands.OptionType.*;
-
-@Service
+@Component
 public class Bot {
 
     private static JDA jda;
-
-    @Value("${app.discord.bot.token}")
     private String token;
-    public Bot() {
-
+    public Bot( @Value("${app.discord.bot.live.token}")
+                String getToken) {
+        this.token = getToken;
         CommandClientBuilder client = new CommandClientBuilder();
         client.addSlashCommands(GetSlashCommands());
         client.setOwnerId("272151652344266762");
@@ -45,52 +36,29 @@ public class Bot {
                 .enableIntents(GatewayIntent.MESSAGE_CONTENT, GatewayIntent.GUILD_MESSAGE_REACTIONS,
                         GatewayIntent.GUILD_MEMBERS)
                 .setActivity(Activity.playing("Booting....."))
-                .addEventListeners(EponaBotApplication.eWaiter, new SlashListener(),
-                        new InteractionListener(), commandClient)
+                .addEventListeners(EponaBotApplication.eWaiter, commandClient)
                 .build();
+
+        jda.getGuilds().forEach(guild -> guild.updateCommands().queue());
+        jda.updateCommands().queue();
     }
 
     private SlashCommand[] GetSlashCommands() {
         List<SlashCommand> commands = new ArrayList<>();
         commands.add(new EyeSpyCommands());
+        commands.add(new RibbonCommands());
+        commands.add(new ProfileCommand());
         return commands.toArray(new SlashCommand[0]);
     }
 
     public static void Setup() {
         // These commands might take a few minutes to be active after creation/update/delete
-        CommandListUpdateAction commands = jda.updateCommands();
-        commands.addCommands(
-                Commands.slash("say", "Makes the bot say what you tell it to")
-                        .addOption(STRING, "content", "What the bot should say", true),
-                Commands.slash("add_ribbon", "add a new ribbon")
-                        .addOption(STRING, "ribbon_image", "URL to image for the ribbon", true)
-                        .addOption(STRING, "ribbon_name", "Name of the ribbon", true)
-                        .addOption(STRING, "ribbon_description", "Description for the ribbon", false)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS)),
-                Commands.slash("delete_ribbon", "delete a ribbon")
-                        .addOption(STRING, "ribbon_name", "Name of the ribbon to delete", true)
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS)),
-                Commands.slash("give_ribbon", "give a ribbon to someone")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS))
-                        .addOption(OptionType.USER, "user", "user to give ribbon to")
-                        .addOption(STRING, "ribbon_name", "name of ribbon to give user", false)
-                        .addOption(INTEGER, "ribbon_id", "Id of ribbon to give user", false),
-                Commands.slash("show_ribbon", "show a ribbon")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS))
-                        .addOption(STRING, "ribbon_name", "name of ribbon to show", true),
-                Commands.slash("profile", "show your profile")
-                        .addOption(USER, "user", "user to show ribbons for. If blank will show your own"
-                        , false),
-                Commands.slash("take_ribbon", "take a ribbon from someone")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS))
-                        .addOption(STRING, "ribbon_name", "Name of the ribbon", true)
-                        .addOption(USER, "user", "user to take ribbon from"),
-                Commands.slash("reset", "reset a users ribbons")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS))
-                        .addOption(USER, "user", "user to reset ribbons for", true),
-                Commands.slash("show_ribbons", "Show all ribbons")
-                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.KICK_MEMBERS))
-        ).queue();
+//        CommandListUpdateAction commands = jda.updateCommands();
+//        commands.addCommands(
+//                Commands.slash("profile", "show your profile")
+//                        .addOption(USER, "user", "user to show ribbons for. If blank will show your own"
+//                        , false)
+//        ).queue();
     }
 
 }
--- src/main/java/com/villchurch/eponabot/slashcommands/ProfileCommand.java
+++ src/main/java/com/villchurch/eponabot/slashcommands/ProfileCommand.java
@@ -1,64 +1,61 @@
 package com.villchurch.eponabot.slashcommands;
 
+import com.jagrosh.jdautilities.command.SlashCommand;
+import com.jagrosh.jdautilities.command.SlashCommandEvent;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
 import com.jagrosh.jdautilities.menu.ButtonEmbedPaginator;
 import com.villchurch.eponabot.EponaBotApplication;
-import com.villchurch.eponabot.Repositories.RibbonRepository;
-import com.villchurch.eponabot.Repositories.UserRibbonsRepository;
+import com.villchurch.eponabot.Helpers.RibbonHelper;
 import com.villchurch.eponabot.models.Ribbon;
 import com.villchurch.eponabot.models.UserRibbons;
-import jakarta.annotation.PostConstruct;
 import net.dv8tion.jda.api.EmbedBuilder;
-import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.entities.User;
-import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
-import org.springframework.beans.factory.annotation.Autowired;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.OptionData;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Component
-public class ProfileCommand {
+public class ProfileCommand extends SlashCommand {
 
-    public static UserRibbonsRepository userRibbonsRepository;
-    public static RibbonRepository ribbonRepository;
-    @Autowired
-    public RibbonRepository getRibbonRepository;
-    @Autowired
-    public UserRibbonsRepository getUserRibbonsRepository;
-
-    @PostConstruct
-    public void init() {
-        ribbonRepository = getRibbonRepository;
-        userRibbonsRepository = getUserRibbonsRepository;
-    }
     private static final EventWaiter eWaiter = EponaBotApplication.eWaiter;
+    public ProfileCommand() {
+        this.name = "profile";
+        this.help = "show a profile for a user";
+        List<OptionData> options = new ArrayList<>();
+        options.add(new OptionData(OptionType.USER, "user", "user to show profile for")
+                .setRequired(false));
+        this.options = options;
+    }
 
-
-    public static void showUsersProfile(SlashCommandInteractionEvent event, User user) {
+    @Override
+    protected void execute(SlashCommandEvent slashCommandEvent) {
+        User user = slashCommandEvent.getUser();
+        if (slashCommandEvent.hasOption("user")) {
+            user = Objects.requireNonNull(slashCommandEvent.getOption("user")).getAsUser();
+        }
         List<Ribbon> matchedRibbons = returnUsersRibbons(user.getId());
-        List<MessageEmbed> ribbons = returnRibbonEmbeds(matchedRibbons, user);
+        List<MessageEmbed> ribbons = returnRibbonEmbeds(matchedRibbons, slashCommandEvent.getUser());
         if (matchedRibbons.size() < 1) {
-            event.reply(user.getName() + " currently has no ribbons").queue();
+            slashCommandEvent.reply(user.getName() + " currently has no ribbons").queue();
         } else {
-            ButtonEmbedPaginator buttonEmbedPaginator = returnButtonEmbedPagiator(ribbons, 1, eWaiter);
+            ButtonEmbedPaginator buttonEmbedPaginator = returnButtonEmbedPagiator(ribbons);
             try {
-                event.replyEmbeds(new EmbedBuilder().setDescription("Checking for ribbons...").build())
-                        .queue(interactionHook -> {
-                            interactionHook.retrieveOriginal()
-                                    .queue(message -> buttonEmbedPaginator.paginate(message, 0));
-                        });
+                slashCommandEvent.replyEmbeds(new EmbedBuilder().setDescription("Checking for ribbons...").build())
+                        .queue(interactionHook -> interactionHook.retrieveOriginal()
+                                .queue(message -> buttonEmbedPaginator.paginate(message, 0)));
             } catch (IllegalArgumentException ex) {
-                event.reply(ex.getMessage()).queue();
+                slashCommandEvent.reply(ex.getMessage()).queue();
             }
         }
     }
-
     private static List<MessageEmbed> returnRibbonEmbeds(List<Ribbon> usersRibbons, User user) {
         List<MessageEmbed> ribbons = new ArrayList<>();
         Collections.reverse(usersRibbons);
@@ -74,39 +71,21 @@ public class ProfileCommand {
         return ribbons;
     }
     private static List<Ribbon> returnUsersRibbons(String userId) {
-        List<UserRibbons> usersRibbons = userRibbonsRepository.findByUserid(userId);
+        List<UserRibbons> usersRibbons = RibbonHelper.getUserRibbons(userId);
         List<Long> ribbonIds = usersRibbons.stream()
                 .map(UserRibbons::getRibbonid)
                 .collect(Collectors.toList());
-        List<Ribbon> allRibbons = ribbonRepository.findAll();
+        List<Ribbon> allRibbons = RibbonHelper.getAllRibbons();
         return allRibbons.stream()
                 .filter(ribbon -> ribbonIds.contains(ribbon.getId()))
                 .collect(Collectors.toList());
     }
-    private static ButtonEmbedPaginator returnButtonEmbedPagiator(List<MessageEmbed> items, int timeout, EventWaiter waiter) {
+    private static ButtonEmbedPaginator returnButtonEmbedPagiator(List<MessageEmbed> items) {
         return new ButtonEmbedPaginator.Builder()
                 .addItems(items)
                 .waitOnSinglePage(true)
-                .setTimeout(timeout, TimeUnit.MINUTES)
-                .setEventWaiter(waiter)
+                .setTimeout(1, TimeUnit.MINUTES)
+                .setEventWaiter(ProfileCommand.eWaiter)
                 .build();
-    }
-    public static void execute(SlashCommandInteractionEvent event) {
-        List<Ribbon> matchedRibbons = returnUsersRibbons(event.getMember().getUser().getId());
-        List<MessageEmbed> ribbons = returnRibbonEmbeds(matchedRibbons, event.getUser());
-        if (matchedRibbons.size() < 1) {
-            event.reply("You currently have no ribbons").queue();
-        } else {
-            ButtonEmbedPaginator buttonEmbedPaginator = returnButtonEmbedPagiator(ribbons, 1, eWaiter);
-            try {
-                event.replyEmbeds(new EmbedBuilder().setDescription("Checking for ribbons...").build())
-                                .queue(interactionHook -> {
-                                    interactionHook.retrieveOriginal()
-                                            .queue(message -> buttonEmbedPaginator.paginate(message, 0));
-                                });
-            } catch (IllegalArgumentException ex) {
-                event.reply(ex.getMessage()).queue();
-            }
-        }
     }
 }
